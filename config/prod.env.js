4229fcffc96a543fe1c70412bc1b22b99f51e49b
Idakwoji Theophilus <pietrosparks@gmail.com>
2018-04-24 23:12:02 +0100

    go

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_URL:JSON.stringify(process.env.API_URL),
+  BASEURL_PROD:JSON.stringify(process.env.BASEURL_PROD),
   MONGOLAB_URI:'"mongodb://verifi:ucholafia1996@ds257589.mlab.com:57589/verifi"'
 }
