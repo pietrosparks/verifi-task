6ab23d68cc236cefb98b2601dcd6e93b43d8a6cb
Idakwoji Theophilus <pietrosparks@gmail.com>
2018-04-24 22:26:33 +0100

    heroku

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,5 +2,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   API_URL:JSON.stringify(process.env.API_URL),
-  MONGOLAB_URI:'"mongodb://pietrosparks:ucholafia1996@ds257589.mlab.com:57589/verifi"'
+  MONGOLAB_URI:'"mongodb://verifi:ucholafia1996@ds257589.mlab.com:57589/verifi"'
 }
