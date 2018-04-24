6c6cc38795111bf031c5475081c9f15b81e3b2e2
Idakwoji Theophilus <pietrosparks@gmail.com>
2018-04-24 18:40:25 +0100

    changes v2

--- api/dbConfig/secrets.js
+++ api/dbConfig/secrets.js
@@ -5,11 +5,11 @@ let MONGO_DB ;
 
 if (process.env.NODE_ENV == 'production'){
     MONGO_DB = process.env.MONGODB_PROD;
-    BASEURL = process.env.BASEURL_PROD
+    BASEURL = process.env.API_BASE_URL_PROD
 }
 else{
     MONGO_DB = process.env.MONGODB_DEV,
-    BASEURL = process.env.BASEURL_DEV
+    BASEURL = process.env.API_BASE_URL
 }
 
 module.exports = {
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,6 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  API_BASE_URL_PROD:'"https://verifi-node.herokuapp.com/api"',
+  MONGO_PROD:'"mongodb://pietrosparks:ucholafia1996@ds257589.mlab.com:57589/verifi"'
 }
